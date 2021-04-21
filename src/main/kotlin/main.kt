459b823064a7227dd0ddfe9f91a07cfba8a88a17
bgchoi205 <choibg205@naver.com>
2021-04-21 20:16:28 +0900

    게시물 명령어 입력

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+
+fun main() {
+    println("==프로그램 시작==")
+
+    while(true){
+        print("명령어 입력 : ")
+        val command = readLine()!!.trim()
+        if(command == "exit"){
+            break
+        }
+
+    }
+
+
+    println("==프로그램 끝==")
+}
